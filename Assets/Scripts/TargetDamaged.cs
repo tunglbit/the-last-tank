818a6d2359f7d6ad59c59a5f023ac8f9d779d79a
Admin <lebuithanhtung@gmail.com>
2018-12-07 01:28:51 +0700

    update 12.6 - add audio :wq

--- Assets/Scripts/TargetDamaged.cs
+++ Assets/Scripts/TargetDamaged.cs
@@ -8,10 +8,23 @@ public class TargetDamaged : MonoBehaviour
     public int hitPoints = 2;
     private int currentHitPoints;
     public GameObject explosionPrefab;
+    private AudioSource explosionAudio;
+    private ParticleSystem explosionParticles;
+
+    [HideInInspector]
+    public Boolean isDead;
 
     // Use this for initialization
+    void Awake()
+    {
+        explosionParticles = Instantiate(explosionPrefab).GetComponent<ParticleSystem>();
+        explosionAudio = explosionParticles.GetComponent<AudioSource>();
+        explosionParticles.gameObject.SetActive(false);
+    }
+
     void Start()
     {
+        isDead = false;
         currentHitPoints = hitPoints;
     }
 
@@ -25,6 +38,11 @@ public class TargetDamaged : MonoBehaviour
         if (currentHitPoints <= 0)
         {
             explosionPrefab = (GameObject)Instantiate(explosionPrefab, transform.position, transform.rotation);
+            isDead = true;
+            explosionParticles.transform.position = transform.position;
+            explosionParticles.gameObject.SetActive(true);
+            explosionParticles.Play();
+            explosionAudio.Play();
             Destroy(gameObject);
         }
     }
